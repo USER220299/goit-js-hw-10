493d84f884a401032bce9508a4527f482fdc9dba
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-08-10 23:34:32 +0300

    edit

---
+++ src/cat-api.js
@@ -0,0 +1,32 @@
+function fetchBreeds(){
+    const BASE_URL = "https://api.thecatapi.com/v1/breeds";
+    const API_KEY = "live_bPw1HefqXKxGjCEcXQfPslG2mdOVn4nvMzRqzHwPA2hQodzh6pHFH6IV0oHopRtT";
+const option = {
+        headers: {
+            'x-api-key': API_KEY
+        }
+    }
+   return fetch(BASE_URL, option).then(response => {
+        if (!response.ok) {    
+            throw new Error(response.statusText)
+        }
+      return response.json()
+})
+}
+
+
+
+ function fetchCatByBreed(breedId) {
+     const BASE_URL = "https://api.thecatapi.com/v1/";
+    const API_KEY = "live_bPw1HefqXKxGjCEcXQfPslG2mdOVn4nvMzRqzHwPA2hQodzh6pHFH6IV0oHopRtT";
+
+
+   return fetch(`${BASE_URL}images/search?api_key=${API_KEY}&breed_ids=${breedId}`).then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText)
+        }
+      return response.json()
+})
+}
+
+export{fetchBreeds, fetchCatByBreed }
--- src/index.js
+++ src/index.js
@@ -1,115 +1,90 @@
-// import axios from "axios";
-// axios.defaults.headers.common["x-api-key"] = "live_bPw1HefqXKxGjCEcXQfPslG2mdOVn4nvMzRqzHwPA2hQodzh6pHFH6IV0oHopRtT";
+
+import { fetchBreeds, fetchCatByBreed }  from "./cat-api.js"
+import SlimSelect from 'slim-select'
+
+new SlimSelect({
+  select: '.breed-select'
+})
 
 const refs = {
-  select:document.querySelector(".breed-select"),
- catInfo: document.querySelector(".cat-info"),
+    select: document.querySelector(".breed-select"),
+    loader: document.querySelector('.loader'),
+    error:document.querySelector('.error'),
+    catInfo: document.querySelector(".cat-info"),
 }
 console.log(refs.select)
+console.log(refs.loader.textContent)
+console.log(refs.error.textContent)
 console.log(refs.catInfo)
 
-
-function fetchBreeds(){
-    const BASE_URL = "https://api.thecatapi.com/v1/breeds";
-    const API_KEY = "live_bPw1HefqXKxGjCEcXQfPslG2mdOVn4nvMzRqzHwPA2hQodzh6pHFH6IV0oHopRtT";
-const option = {
-        headers: {
-            'x-api-key': API_KEY
-        }
-    }
-   return fetch(BASE_URL, option).then(response => {
-        if (!response.ok) {    
-            throw new Error(response.statusText)
-        }
-      return response.json()
-})
-}
-fetchBreeds().
-    then(data => {
+refs.select.hidden = true
+refs.error.hidden = true
+ 
+fetchBreeds().then(data => {
+  refs.select.hidden = false;
+  refs.loader.hidden = true;
         data = data.map((({ id, name }) => {
             return `<option value="${id}">${name}</option>`
        })).join('')
     refs.select.insertAdjacentHTML('afterbegin',data);
     })
-    .catch(error => { console.log(error) });
+  .catch(error => {
+      refs.loader.hidden = true;
+      refs.error.hidden = false;
+    console.log(error)
+  });
 
 
-
-function fetchCatByBreed(breedId) {
-     const BASE_URL = "https://api.thecatapi.com/v1/";
-    const API_KEY = "live_bPw1HefqXKxGjCEcXQfPslG2mdOVn4nvMzRqzHwPA2hQodzh6pHFH6IV0oHopRtT";
+refs.select.addEventListener('input', searchCat);
 
 
-   return fetch(`${BASE_URL}images/search??api_key=${API_KEY}&breed_ids=${breedId}`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.statusText)
-        }
-      return response.json()
-})
-}
     
-fetchCatByBreed("abys")
-    .then(data => {
-        data = data.map(({ url, breeds: { name, description, temperament } }) => {
-      return `<img src="${url}" alt="" />
-     <h2>${ name }</h2>
-      <p>${ description }</p>
-      <h3>Temperament</h3>
-     <p>${{  temperament }}</p> ` 
-    }).join('')
-     refs.catInfo.insertAdjacentHTML('afterbegin',data);
-    })
-    .catch(error => { console.log(error) });
+function searchCat(evt) {
+  evt.preventDefault();
 
-
-
-
-// refs.select.addEventListener("submit", searchCat);
-// // refs.catInfo.addEventListener("submit", searchCat);
-
-// function searchCat(evt) {
-//     evt.preventDefoult();
-
-//     const { breedId } = evt.currentTarget.elements;
-
-// fetchCatByBreed(breedId.value).then(data => { console.log(data)}).catch(error => { console.log(error) });
-// }
-
-// function createMarkup(arr) {
-//   const marup = el =>  arr.map(({ url, breeds: { name, description, temperament } }) => {return `
-//      <img src="${url}" alt="${name}" />
-//       <h2>${ name }</h2>
-//       <p>${description }</p>
-//       <h3>Temperament</h3>
-//       <p>${{ temperament }}</p> `
-    
-// }).join("")
-//    refs.catInfo.innerHTML = marup(arr[0]);
-// }
-// createMarkup("abys")
-// catInfo.insertAdjacentHTML('afterbegin', item);
-
-
-
-
-// then((data) => {
-   
-//    data = data.filter(img=> img.image?.url)
+  refs.catInfo.innerHTML = '';
+  refs.loader.hidden = false;
   
-//     console.log(data)
-   
-//    for (let i = 0; i < data.length; i+=1) {
-//        const breed = data[i];
-//        console.log(breed)
-//     let option = document.createElement('option');
-     
-//     console.log(option)
-   
-//     option.value = i;
-//     option.innerHTML = `${breed.name}`;
-       
-// const select = document.querySelector('.breed_selector');
-//     select.insertAdjacentHTML('afterbegin',option)
-//     }
+    const { value }= evt.target;
+   fetchCatByBreed(value)
+     .then(data => {
+      refs.loader.hidden = true;
+       createMarkup(data)
+     })
+     .catch(error => {
+       console.log(error)
+     });
+}
 
-// })
+function createMarkup(arr) {
+  
+ const card =  arr.map((el) => {
+    return `<l><img src="${el.url}" alt="${el.breeds[0].name}"  width="${el.width}" height="${el.height}"/>
+      <h2>${el.breeds[0].name}</h2>
+      <p>${el.breeds[0].description}</p>
+      <h3>Temperament</h3>
+      <p>${el.breeds[0].temperament}</p></l>`
+    
+ }).join("")
+ 
+  refs.catInfo.insertAdjacentHTML('afterbegin', card);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
