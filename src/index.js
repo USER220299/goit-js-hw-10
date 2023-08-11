22f6f03e353214c5d60b58b84954b32864cfb959
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-08-11 17:06:17 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
-import { fetchBreeds, fetchCatByBreed }  from "./cat-api.js"
-import SlimSelect from 'slim-select'
+function slim() {
+     new SlimSelect({
+     select: refs.select,
+ })
+  
 
-new SlimSelect({
-  select: '.breed-select'
-})
+}
+
+
 
 const refs = {
     select: document.querySelector(".breed-select"),
@@ -12,6 +19,7 @@ const refs = {
     error:document.querySelector('.error'),
     catInfo: document.querySelector(".cat-info"),
 }
+
 console.log(refs.select)
 console.log(refs.loader.textContent)
 console.log(refs.error.textContent)
@@ -19,18 +27,28 @@ console.log(refs.catInfo)
 
 refs.select.hidden = true
 refs.error.hidden = true
- 
+refs.loader.textContent = "";
+
+  
+
 fetchBreeds().then(data => {
+
   refs.select.hidden = false;
-  refs.loader.hidden = true;
-        data = data.map((({ id, name }) => {
-            return `<option value="${id}">${name}</option>`
-       })).join('')
-    refs.select.insertAdjacentHTML('afterbegin',data);
+  // refs.loader.hidden = true;
+  refs.loader.classList.replace("loader", "loader-hidden");
+
+   data = data.map((({ id, name }) => {
+     return `<option value="${id}">${name}</option>`
+   })).join('')
+  
+  refs.select.insertAdjacentHTML('afterbegin', data);
+  
     })
   .catch(error => {
-      refs.loader.hidden = true;
-      refs.error.hidden = false;
+    // refs.loader.hidden = true;
+    refs.loader.classList.replace("loader", "loader-hidden");
+    // refs.error.hidden = false;
+  Report.failure(refs.error.textContent);
     console.log(error)
   });
 
@@ -41,33 +59,39 @@ refs.select.addEventListener('input', searchCat);
     
 function searchCat(evt) {
   evt.preventDefault();
-
   refs.catInfo.innerHTML = '';
-  refs.loader.hidden = false;
+  // refs.loader.hidden = false;
+    refs.loader.classList.replace("loader-hidden", "loader");
+  
   
     const { value }= evt.target;
    fetchCatByBreed(value)
      .then(data => {
-      refs.loader.hidden = true;
+      // refs.loader.hidden = true;
+        refs.loader.classList.replace("loader", "loader-hidden");
        createMarkup(data)
+      
      })
      .catch(error => {
        console.log(error)
      });
+
 }
 
 function createMarkup(arr) {
-  
+
  const card =  arr.map((el) => {
-    return `<l><img src="${el.url}" alt="${el.breeds[0].name}"  width="${el.width}" height="${el.height}"/>
-      <h2>${el.breeds[0].name}</h2>
-      <p>${el.breeds[0].description}</p>
+    return `<l class = "card-cat"><img class = "image" src="${el.url}" alt="${el.breeds[0].name}  width="${el.width}" height="${el.height}"" />
+      <div class="card-info"><h2 class = "name">${el.breeds[0].name}</h2>
+      <p class="description">${el.breeds[0].description}</p>
       <h3>Temperament</h3>
-      <p>${el.breeds[0].temperament}</p></l>`
+      <p class = "temperament">${el.breeds[0].temperament}</p>
+      </div></l>`
     
  }).join("")
- 
+  
   refs.catInfo.insertAdjacentHTML('afterbegin', card);
+
 }
 
 
